8b1f44fd397761482795835792c43c78fb7eb4b8
ivankosic <ivankosic55@gmail.com>
2025-04-05 20:16:48 +0200

    Dodate klase Outcome i ScientificField

---
+++ src/main/java/main/model/Outcome.java
@@ -0,0 +1,98 @@
+package main.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Outcome {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Lob
+	@Column(nullable = false)
+	private String description;
+	
+	@OneToOne
+	@Column(nullable = true)
+	private EducationGoal educationGoal;
+	
+	@OneToOne
+	@Column(nullable = false)
+	private TeachingMaterial teachingMaterial;
+	
+	@OneToOne
+	@Column(nullable = false)
+	private Subject subject;
+	
+	@Column(nullable = false)
+	private Boolean active;
+	
+	public Outcome() {}
+
+	public Outcome(Long id, String description, EducationGoal educationGoal, TeachingMaterial teachingMaterial,
+			Subject subject, Boolean active) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.educationGoal = educationGoal;
+		this.teachingMaterial = teachingMaterial;
+		this.subject = subject;
+		this.active = active;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public EducationGoal getEducationGoal() {
+		return educationGoal;
+	}
+
+	public void setEducationGoal(EducationGoal educationGoal) {
+		this.educationGoal = educationGoal;
+	}
+
+	public TeachingMaterial getTeachingMaterial() {
+		return teachingMaterial;
+	}
+
+	public void setTeachingMaterial(TeachingMaterial teachingMaterial) {
+		this.teachingMaterial = teachingMaterial;
+	}
+
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+}
---
+++ src/main/java/main/model/ScientificField.java
@@ -0,0 +1,75 @@
+package main.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class ScientificField {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Lob
+	@Column(nullable = false)
+	private String name;
+	
+	@ManyToOne
+	@Column(nullable = false)
+	private Teacher teacher; // Jedan nastavnik moze imati samo jedno zvanje u jednoj oblasti, ali moze biti biran u vise oblasti
+	
+	@Column(nullable = false)
+	private Boolean active;
+
+	public ScientificField() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ScientificField(Long id, String name, Teacher teacher, Boolean active) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.teacher = teacher;
+		this.active = active;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+	
+}	
