07d6e92ac2d8621b0b7ab2a00d2e3fcbacd2664f
ivankosic <ivankosic55@gmail.com>
2025-04-05 20:39:16 +0200

    Dodate klase Title i TitleType

---
+++ src/main/java/main/model/Title.java
@@ -0,0 +1,103 @@
+package main.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Title {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private Date dateElected;
+	
+	@Column(nullable = false)
+	private Date dateAbolished;
+	
+	@Column(nullable = false)
+	@OneToOne
+	private ScientificField scientificField;
+	
+	@Column(nullable = false)
+	@OneToOne
+	private TitleType titleType;
+	
+	@Column(nullable = false)
+	private Boolean active;
+
+	public Title() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Title(Long id, Date dateElected, Date dateAbolished, ScientificField scientificField, TitleType titleType,
+			Boolean active) {
+		super();
+		this.id = id;
+		this.dateElected = dateElected;
+		this.dateAbolished = dateAbolished;
+		this.scientificField = scientificField;
+		this.titleType = titleType;
+		this.active = active;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDateElected() {
+		return dateElected;
+	}
+
+	public void setDateElected(Date dateElected) {
+		this.dateElected = dateElected;
+	}
+
+	public Date getDateAbolished() {
+		return dateAbolished;
+	}
+
+	public void setDateAbolished(Date dateAbolished) {
+		this.dateAbolished = dateAbolished;
+	}
+
+	public ScientificField getScientificField() {
+		return scientificField;
+	}
+
+	public void setScientificField(ScientificField scientificField) {
+		this.scientificField = scientificField;
+	}
+
+	public TitleType getTitleType() {
+		return titleType;
+	}
+
+	public void setTitleType(TitleType titleType) {
+		this.titleType = titleType;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+	
+	
+	
+}
