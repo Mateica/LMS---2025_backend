3803182e77157fe8aeef6cdfa99bd9ece1448b55
ivankosic <ivankosic55@gmail.com>
2025-04-09 17:54:07 +0200

    Zapocet servis za autentifikaciju i uradjen DTO za registrovanog korisnika

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -2,6 +2,9 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class App {
@@ -9,6 +12,11 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
