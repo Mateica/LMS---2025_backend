0408d493580290df2657c5e1a7f5b0bd29cacde1
ivankosic <ivankosic55@gmail.com>
2025-04-12 12:04:44 +0200

    Dodato logičko brisanje u repozitorijum za registrovanog korisnika

--- src/main/java/main/repository/RegisteredUserRepository.java
+++ src/main/java/main/repository/RegisteredUserRepository.java
@@ -1,5 +1,6 @@
 package main.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,6 @@ import main.model.RegisteredUser;
 
 @Repository
 public interface RegisteredUserRepository extends CrudRepository<RegisteredUser, Long>, PagingAndSortingRepository<RegisteredUser, Long> {
-
+	@Query("UPDATE RegisteredUser r SET r.active = false WHERE r.id = :id")
+	public void softDelete(Long id);	
 }
