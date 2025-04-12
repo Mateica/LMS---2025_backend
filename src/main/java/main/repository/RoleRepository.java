5b0f2aec87464404576bdef2f66eab45ceccd711
ivankosic <ivankosic55@gmail.com>
2025-04-12 11:59:48 +0200

    Dodato sortiranje i paginacija u RoleRepository

--- src/main/java/main/repository/RoleRepository.java
+++ src/main/java/main/repository/RoleRepository.java
@@ -3,12 +3,13 @@ package main.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import main.model.Role;
 
 @Repository
-public interface RoleRepository extends CrudRepository<Role, Long> {
+public interface RoleRepository extends CrudRepository<Role, Long>, PagingAndSortingRepository<Role, Long> {
 	@Query("UPDATE Role r SET r.active = false WHERE r.id = :id")
 	public void softDelete(Long id);
 }
