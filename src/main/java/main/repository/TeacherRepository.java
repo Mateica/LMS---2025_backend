138170d23f4ba5437aad55e6051e950e11a77421
ivankosic <ivankosic55@gmail.com>
2025-04-12 12:06:21 +0200

    Dodato logičko brisanje u repozitorijum za nastavnika

--- src/main/java/main/repository/TeacherRepository.java
+++ src/main/java/main/repository/TeacherRepository.java
@@ -1,5 +1,6 @@
 package main.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,6 @@ import main.model.Teacher;
 
 @Repository
 public interface TeacherRepository extends CrudRepository<Teacher,Long>, PagingAndSortingRepository<Teacher,Long> {
-
+	@Query("UPDATE Teacher t SET t.active = false WHERE t.id = :id")
+	public void softDelete(Long id);
 }
