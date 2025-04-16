a8e7c275612156df66b1b2b03b47630d8d3e0051
ivankosic <ivankosic55@gmail.com>
2025-04-16 19:01:23 +0200

    Dodati DTO za drzavu i mesto

---
+++ src/main/java/main/dto/CountryDTO.java
@@ -0,0 +1,58 @@
+package main.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountryDTO {
+	private Long id;
+	private String name;
+	private List<PlaceDTO> places = new ArrayList<PlaceDTO>();
+	private Boolean active;
+	
+	
+	public CountryDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public List<PlaceDTO> getPlaces() {
+		return places;
+	}
+
+
+	public void setPlaces(List<PlaceDTO> places) {
+		this.places = places;
+	}
+
+
+	public Boolean getActive() {
+		return active;
+	}
+
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+	
+}
