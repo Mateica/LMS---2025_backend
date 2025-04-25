c3ce51035bfe9935d5958c54f706db8d7f0539ad
ivankosic <ivankosic55@gmail.com>
2025-04-25 19:11:03 +0200

    Dodat repozitorijum za tip evaluacije znanja

---
+++ src/main/java/main/repository/EvaluationTypeRepository.java
@@ -0,0 +1,14 @@
+package main.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import main.model.EvaluationType;
+
+@Repository
+public interface EvaluationTypeRepository extends CrudRepository<EvaluationType, Long>, PagingAndSortingRepository<EvaluationType, Long> {
+	@Query("UPDATE EvaluationType t SET t.active = false WHERE t.id = :id")
+	public void softDelete(Long id);
+}
