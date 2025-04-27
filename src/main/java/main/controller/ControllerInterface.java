8829f06060fbe598494423cab62d6f30d888ed59
ivankosic <ivankosic55@gmail.com>
2025-04-27 20:24:23 +0200

    Dodate metode za paginaciju u interfejse za kontrolere i servise

--- src/main/java/main/controller/ControllerInterface.java
+++ src/main/java/main/controller/ControllerInterface.java
@@ -1,9 +1,16 @@
 package main.controller;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ControllerInterface<T>{
 	public ResponseEntity<Iterable<T>> findAll();
+	public ResponseEntity<Page<T>> findAll(
+			@RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "true") boolean ascending);
 	public ResponseEntity<T> findById(Long id);
 	public ResponseEntity<T> create(T t);
 	public ResponseEntity<T> update(T t,Long id);
--- src/main/java/main/service/ServiceInterface.java
+++ src/main/java/main/service/ServiceInterface.java
@@ -2,8 +2,12 @@ package main.service;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ServiceInterface<T>{
 	public Iterable<T> findAll();
+	public Page<T> findAll(Pageable pageable);
 	public Optional<T> findById(Long id);
 	public T create(T t);
 	public T update(T t);
