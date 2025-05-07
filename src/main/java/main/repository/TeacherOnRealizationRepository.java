fa37b618f71198708abb2dd2bb7ec048d611693c
ivankosic <ivankosic55@gmail.com>
2025-05-07 22:35:01 +0200

    Dodat repozitorijum za nastavnika na realizaciji

---
+++ src/main/java/main/repository/TeacherOnRealizationRepository.java
@@ -0,0 +1,14 @@
+package main.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import main.model.Teacher;
+
+@Repository
+public interface TeacherOnRealizationRepository extends CrudRepository<Teacher,Long>, PagingAndSortingRepository<Teacher,Long> {
+	@Query("UPDATE TeacherOnRealization t SET t.active = false WHERE t.id = :id")
+	public void softDelete(Long id);
+}
