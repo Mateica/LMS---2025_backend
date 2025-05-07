9853242970db6737559dc7bf53d16f5a6669fbbf
ivankosic <ivankosic55@gmail.com>
2025-05-07 22:42:50 +0200

    Dodat servis za nastavnika na realizaciji

--- src/main/java/main/repository/TeacherOnRealizationRepository.java
+++ src/main/java/main/repository/TeacherOnRealizationRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import main.model.Teacher;
+import main.model.TeacherOnRealization;
 
 @Repository
-public interface TeacherOnRealizationRepository extends CrudRepository<Teacher,Long>, PagingAndSortingRepository<Teacher,Long> {
+public interface TeacherOnRealizationRepository extends CrudRepository<TeacherOnRealization,Long>, PagingAndSortingRepository<TeacherOnRealization,Long> {
 	@Query("UPDATE TeacherOnRealization t SET t.active = false WHERE t.id = :id")
 	public void softDelete(Long id);
 }
---
+++ src/main/java/main/service/TeacherOnRealizationService.java
@@ -0,0 +1,72 @@
+package main.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import main.model.StudyProgramme;
+import main.model.TeacherOnRealization;
+import main.repository.TeacherOnRealizationRepository;
+
+@Service
+public class TeacherOnRealizationService implements ServiceInterface<TeacherOnRealization> {
+	@Autowired
+	private TeacherOnRealizationRepository repo;
+
+	@Override
+	public Iterable<TeacherOnRealization> findAll() {
+		// TODO Auto-generated method stub
+		return this.repo.findAll();
+	}
+
+	@Override
+	public Page<TeacherOnRealization> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return this.repo.findAll(pageable);
+	}
+
+	@Override
+	public Optional<TeacherOnRealization> findById(Long id) {
+		// TODO Auto-generated method stub
+		return this.repo.findById(id);
+	}
+
+	@Override
+	public TeacherOnRealization create(TeacherOnRealization t) {
+		// TODO Auto-generated method stub
+		return this.repo.save(t);
+	}
+
+	@Override
+	public TeacherOnRealization update(TeacherOnRealization t) {
+		// TODO Auto-generated method stub
+		if(findById(t.getId()).isPresent()) {
+			return this.repo.save(t);
+		}
+		
+		throw new RuntimeException("No teacher with such ID!");
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void softDelete(Long id) {
+		// TODO Auto-generated method stub
+		TeacherOnRealization t = repo.findById(id).orElse(null);
+		
+		if(t == null) {
+			throw new RuntimeException("No study programme with such ID!");
+		}
+		
+		t.setActive(false);
+		repo.save(t);
+	}
+	
+}
