0549b7a2687233f8c87003f2de50717191225cc9
ivankosic <ivankosic55@gmail.com>
2025-05-10 18:47:06 +0200

    Dodate metode za logicko brisanje u repozitorijume za predmet i realizaciju predmeta

--- src/main/java/main/repository/SubjectRealizationRepository.java
+++ src/main/java/main/repository/SubjectRealizationRepository.java
@@ -1,5 +1,6 @@
 package main.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,6 @@ import main.model.SubjectRealization;
 
 @Repository
 public interface SubjectRealizationRepository extends CrudRepository<SubjectRealization, Long>, PagingAndSortingRepository<SubjectRealization, Long> {
-
+	@Query("UPDATE SubjectRealization t SET t.active = false WHERE t.id = :id")
+	public void softDelete(Long id);
 }
