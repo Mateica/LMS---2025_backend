03d1e77495e26387a3df2fc77dbe9ced3224ba99
ivankosic <ivankosic55@gmail.com>
2025-05-20 18:15:43 +0200

    Dodat servis za studenta

---
+++ src/main/java/main/service/StudentService.java
@@ -0,0 +1,72 @@
+package main.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import main.model.Student;
+import main.repository.StudentRepository;
+
+@Service
+public class StudentService implements ServiceInterface<Student>{
+	@Autowired
+	private StudentRepository repo;
+
+	@Override
+	public Iterable<Student> findAll() {
+		// TODO Auto-generated method stub
+		return this.repo.findAll();
+	}
+
+	@Override
+	public Page<Student> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return this.repo.findAll(pageable);
+	}
+
+	@Override
+	public Optional<Student> findById(Long id) {
+		// TODO Auto-generated method stub
+		return this.repo.findById(id);
+	}
+
+	@Override
+	public Student create(Student t) {
+		// TODO Auto-generated method stub
+		return this.repo.save(t);
+	}
+
+	@Override
+	public Student update(Student t) {
+		// TODO Auto-generated method stub
+		if(findById(t.getId()).isPresent()) {
+			return this.repo.save(t);
+		}
+		
+		throw new RuntimeException("No student with such ID!");
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void softDelete(Long id) {
+		// TODO Auto-generated method stub
+		Student s = findById(id).orElse(null);
+		
+		if(s != null) {
+			s.setActive(false);
+			repo.save(s);
+		}
+		
+		throw new RuntimeException("No student with such ID!");
+	}
+	
+	
+}
